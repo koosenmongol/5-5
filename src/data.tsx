b29126d9beb5bb5b4c51eafd720dc57c36f49709
koosenmongol <koosenmongol@gmail.com>
2024-12-17 09:18:11 +0800

    data

--- src/data.tsx
+++ src/data.tsx
@@ -1,5 +1,36 @@
 export const postData = [
-  { id: 1, desc: "My post1", img: require("../assets/images/Post1.png") },
+  {
+    id: 1,
+    desc: "My post1",
+    img: require("../assets/images/Post1.png"),
+    comments: [
+      {
+        id: "User1",
+        idProfileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+        comment: "Comment 1",
+      },
+      {
+        id: "User2",
+        idProfileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+        comment: "Comment 2",
+      },
+      {
+        id: "User3",
+        idProfileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+        comment: "Comment 3",
+      },
+      {
+        id: "User4",
+        idProfileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+        comment: "Comment 4",
+      },
+      {
+        id: "User5",
+        idProfileImg: "https://randomuser.me/api/portraits/men/75.jpg",
+        comment: "Comment 5",
+      },
+    ],
+  },
   { id: 2, desc: "My post2", img: require("../assets/images/Post2.png") },
   { id: 3, desc: "My post3", img: require("../assets/images/Post3.png") },
   { id: 4, desc: "My post1", img: require("../assets/images/Post4.png") },
