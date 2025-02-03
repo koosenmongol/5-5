2a77a9b21e473a0c4a7899495de62d0e5f47c628
koosenmongol <koosenmongol@gmail.com>
2025-02-03 09:10:46 +0800

    Lesson1

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,10 +1,28 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import React from "react";
 import PagerView from "react-native-pager-view";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Post from "@/components/Post";
+
 const index = () => {
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
+      <View style={styles.homeHeader}>
+        <View>
+          <Image source={require("../../assets/home/instaLoco.png")} />
+        </View>
+        <View style={styles.headerLoco}>
+          <AntDesign name="hearto" size={24} color="black" />
+          <Image source={require("../../assets/home/directMessage.png")} />
+          <AntDesign name="plussquareo" size={24} color="black" />
+        </View>
+      </View>
+      <View style={styles.homeStory}>
+        <Image source={require("../../assets/home/Story user.png")} />
+        <Image source={require("../../assets/home/Story user (1).png")} />
+        <Image source={require("../../assets/home/Story user (2).png")} />
+        <Image source={require("../../assets/home/Story user (3).png")} />
+      </View>
     </View>
   );
 };
@@ -13,9 +31,24 @@ export default index;
 
 const styles = StyleSheet.create({
   container: {
-    width: 390,
-  },
-  pagerView: {
+    width: "100%",
     flex: 1,
+  },
+  homeHeader: {
+    width: "100%",
+    padding: 5,
+    height: 40,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  headerLoco: {
+    flexDirection: "row",
+    gap: 20,
+  },
+  homeStory: {
+    flexDirection: "row",
+    width: "100%",
+    height: 105,
+    gap: 12,
   },
 });
