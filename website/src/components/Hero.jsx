073492c638d0b3891565cb8727e379b3b65d6789
koosenmongol <koosenmongol@gmail.com>
2025-04-28 09:50:28 +0800

    Purpose&Features section

--- website/src/components/Hero.jsx
+++ website/src/components/Hero.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import heroImage from "../assets/hero-image.png";
 const Hero = () => {
   return (
-    <section className="container mx-auto flex flex-col md:flex-row pt-44 pb-6 px-4 sm:px-6 lg:px-8">
+    <section
+      className="container mx-auto flex flex-col md:flex-row pt-44 pb-6 px-4 sm:px-6 lg:px-8"
+      id="home"
+    >
       {/* left */}
       <div className="w-full md:w-1/2 space-y-8">
         <div className="flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
